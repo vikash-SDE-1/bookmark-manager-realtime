736acde68d03106dcb898a0e6c4763e47808dd9e
svikash1651@gmail.com <svikash1651@gmail.com>
2026-02-18 21:50:05 +0530

    fix oauth redirect

--- app/page.tsx
+++ app/page.tsx
@@ -78,7 +78,7 @@ export default function Home() {
     await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: 'http://localhost:3000'
+        redirectTo:window.location.origin
       }
     })
   }
